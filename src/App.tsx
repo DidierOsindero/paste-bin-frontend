228cec4960d618c6607bac0eef55b4fb7f6ef4a1
Maria <mtens17@gmail.com>
2023-01-03 16:43:00 +0000

    Add component and baseUrl

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { SavePaste } from "./components/SavePastePage";
 import { greet } from "./utils/greet";
 
+
+export const baseUrl = process.env.NODE_ENV === "production"
+? "https://didier-maria-pastebin-api.onrender.com"
+: "http://localhost:4000"
+
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return <SavePaste/>
 }
 
 export default App;
