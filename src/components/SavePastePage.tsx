ad012dd63753a8633bc3c207a883c80cf877da1e
Didier Osindero <didierosindero@gmail.com>
2023-01-04 15:12:30 +0000

    Format and check for empty content

--- src/components/SavePastePage.tsx
+++ src/components/SavePastePage.tsx
@@ -1,32 +1,49 @@
-import { useState } from "react"
-import axios from "axios"; 
+import { useState } from "react";
+import axios from "axios";
 import { baseUrl } from "../App";
 
 interface INewPaste {
-    title: string;
-    content: string; 
+  title: string;
+  content: string;
 }
 
-export function SavePaste(): JSX.Element{
-    const [pasteData, setPasteData] = useState<INewPaste>({title: "", content: ""})
-    const updatePasteTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPasteData(prev => {return {...prev, title: e.target.value}})
-    }
-    const updatePasteContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setPasteData(prev => {return {...prev, content: e.target.value}})
-    }
-    
-    const handleSubmit = ()=> {
-        axios.post(baseUrl+"/pastes", pasteData);
-    }
-    return (
-        <div className="ctn-save-pastes-page">
-        <form onSubmit = {handleSubmit}>
-        <input className="title-input"
-        onChange = {updatePasteTitle} placeholder = "Write your title" value = {pasteData.title}/> 
-        <textarea className="content-input" required onChange = {updatePasteContent} placeholder = "Write your content" value = {pasteData.content}/>
-        <input className="input-submit" type = "submit" value="submit"/>
-        </form>
-        </div>
-    )
+export function SavePaste(): JSX.Element {
+  const [pasteData, setPasteData] = useState<INewPaste>({
+    title: "",
+    content: "",
+  });
+  const updatePasteTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasteData((prev) => {
+      return { ...prev, title: e.target.value };
+    });
+  };
+  const updatePasteContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPasteData((prev) => {
+      return { ...prev, content: e.target.value };
+    });
+  };
+
+  const handleSubmit = () => {
+    axios.post(baseUrl + "/pastes", pasteData);
+  };
+  return (
+    <div className="ctn-save-pastes-page">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="title-input"
+          onChange={updatePasteTitle}
+          placeholder="Write your title"
+          value={pasteData.title}
+        />
+        <textarea
+          className="content-input"
+          required
+          onChange={updatePasteContent}
+          placeholder="Write your content"
+          value={pasteData.content}
+        />
+        <input className="input-submit" type="submit" value="submit" disabled={pasteData.content.trim() === ""}/>
+      </form>
+    </div>
+  );
 }
