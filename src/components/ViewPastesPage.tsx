3fb0651cfb403bdaca1328926b8ba7aaae952455
Didier Osindero <didierosindero@gmail.com>
2023-01-04 11:33:57 +0000

    Create functioning view pastes component

--- src/components/ViewPastesPage.tsx
+++ src/components/ViewPastesPage.tsx
@@ -1,5 +1,41 @@
-export function SavePaste(): JSX.Element{
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { baseUrl } from "../App";
+
+interface IPaste {
+    id: string;
+    title: string;
+    content: string; 
+    time: string;
+}
+
+export function ViewPastesPage(): JSX.Element{
+    const [pasteArray, setPasteArray] = useState<IPaste[]>([]);
+
+    async function getRecentPastes () {
+        const response = await axios.get(baseUrl+"/pastes")
+        const data = response.data;
+        setPasteArray(data);
+    }
+
+    useEffect(() => {
+        getRecentPastes();
+    }, []);
+    console.log("TIME", pasteArray[0])
+
     return (
-        <input placeholder = "Write your title"></input>
+        <div className="ctn-paste">
+        {pasteArray.map((paste, index) => {
+            return (
+                <div key={index} className="paste">
+                    <h1>
+                        {paste.title}
+                    </h1>
+                    <p>{paste.content}</p>
+                    <small>Date: {paste.time.substring(0,10)}, Time: {paste.time.substring(11,16)}</small>
+                </div>
+            )
+        })}
+        </div>
     )
 }
